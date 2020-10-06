9f77485bea960a135c96a061bbb6ec6cabf84733
Sai Sandeep Jyothula <saisandeep.jyothula@sjsu.edu>
2020-10-05 22:46:02 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  secret: "cmpe273_kafka_passport_mongo",
+  secret: "",
   mongoURI:
-    "mongodb+srv://sandeep:7777@sandeep-bpru9.mongodb.net/test?retryWrites=true&w=majority",
+    "",
   frontEndURI: "http://localhost:3000"
 };
